761899a786045a1281ce4853b93ff424796b9311
jayesh <jayeshborse0705@gmail.com>
2022-09-16 20:46:24 -0700

    [jayesh] insert  item Element BST

---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree<int> binarySearch = new BinaryTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+        }
+    }
+}
